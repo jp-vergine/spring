9c627257df35cb2f5287cb79219e11b7d038dcee
jpvergine <jpvergine@gmail.com>
2015-01-05 18:00:14 +0100

    spring security update

--- tutorials-spring-web/src/main/java/com/tutorials/spring/configuration/security/SecurityConfig.java
+++ tutorials-spring-web/src/main/java/com/tutorials/spring/configuration/security/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@Configuration 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
