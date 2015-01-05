1b67cccaa9c4e2415eccab5ce2f682f822a4687e
jpvergine <jpvergine@gmail.com>
2015-01-05 17:59:33 +0100

    new version

--- tutorials-spring-web/src/main/java/com/tutorials/spring/configuration/security/SecurityConfig.java
+++ tutorials-spring-web/src/main/java/com/tutorials/spring/configuration/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -25,5 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and().formLogin();
  
 	}
+    
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception
+    {
+        webSecurity.ignoring().antMatchers("/upload").antMatchers("/spring-websocket/**");
+    }
     
 }
